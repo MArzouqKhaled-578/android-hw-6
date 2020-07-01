db94dd318f8748368052b805b67dd2e4a062a7d8
مرزوق خالد مرزوق -578 <marzouq.k911@gmail.com>
2020-07-01 19:22:16 +0300

    solve ;)

---
+++ hw6/app/src/main/java/com/example/hw5/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.hw5;
+
+
+            import androidx.appcompat.app.AppCompatActivity;
+
+            import android.content.Intent;
+            import android.os.Bundle;
+            import android.view.View;
+            import android.widget.Button;
+            import android.widget.EditText;
+
+            import java.security.PublicKey;
+
+public class MainActivity extends AppCompatActivity {
+    private Button Button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText M= findViewById(R.id.name);
+        final EditText Age = findViewById(R.id.editText2);
+        final EditText job = findViewById(R.id.editText3);
+        final EditText phone = findViewById(R.id.editText4);
+        final EditText email = findViewById(R.id.editText5);
+        final Button NEXT = findViewById(R.id.button);
+
+        NEXT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = M.getText().toString();
+                String editText2 = Age.getText().toString();
+                String editText3 = job.getText().toString();
+                String editText4 = phone.getText().toString();
+                String editText5 = email.getText().toString();
+                Intent i = new Intent(MainActivity.this, MainActivity.class);
+                i.putExtra("Name", name);
+                i.putExtra("Age", editText2);
+                i.putExtra("jop", editText3);
+                i.putExtra("phone", editText4);
+                i.putExtra("email", editText5);
+                startActivity(i);
+            }
+        });
+
+    }
+}
+
+
+
+
+
---
+++ hw6/app/src/main/java/com/example/hw5/MainActivity2.java
@@ -0,0 +1,31 @@
+package com.example.hw5;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.jar.Attributes;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        TextView m = findViewById(R.id.name);
+        TextView Age = findViewById(R.id.editText2);
+        TextView jop = findViewById(R.id.editText3);
+        TextView phone = findViewById(R.id.editText4);
+        TextView email = findViewById(R.id.editText5);
+
+        Bundle b = getIntent().getExtras();
+
+
+        String name = b.getString("Name");
+        b.getInt("Age");
+        String editText3 = b.getString("jop");
+        b.getInt("phone");
+        String editText5 = b.getString("email");
+
+    }
+}
